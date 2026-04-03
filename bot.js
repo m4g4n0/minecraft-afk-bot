e18ba2b41ef2f9025a6d38eae818ba2510e44de4
m4g4n0 <gmaganobulhao@gmail.com>
2026-04-03 18:52:43 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+const mineflayer = require('mineflayer')
+
+function createBot(){
+
+const bot = mineflayer.createBot({
+  host: 'terra-dos-cubos.aternos.me',
+  port: 25565,
+  username: 'BotAFK'
+})
+
+bot.on('spawn', () => {
+
+console.log('Bot entrou no servidor')
+
+setInterval(() => {
+
+bot.setControlState('jump', true)
+
+setTimeout(() => {
+bot.setControlState('jump', false)
+}, 500)
+
+}, 5000)
+
+})
+
+bot.on('end', () => {
+console.log('Reconectando...')
+setTimeout(createBot, 5000)
+})
+
+}
+
+createBot()
