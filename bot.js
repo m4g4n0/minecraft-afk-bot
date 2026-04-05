30878651e4b9c54690f9cf2f6f6ec1f037ee7108
m4g4n0 <gmaganobulhao@gmail.com>
2026-04-05 09:11:18 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const bot = mineflayer.createBot({
   host: 'terra-dos-cubos.aternos.me',
   port: 55157,
   username: 'BotAFK',
-  version: '1.21.11'
+  version: '1.21.7'
     })
 
 bot.on('spawn', () => {
